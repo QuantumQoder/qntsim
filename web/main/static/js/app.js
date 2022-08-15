d3e7fd40046b73432fcaf36d57624cce0a44e3d1
VashuKochar <vashukochar@gmail.com>
2022-08-15 16:32:25 +0000

    Removed cc add row button

--- web/main/static/js/app.js
+++ web/main/static/js/app.js
@@ -341,24 +341,3 @@ function fixButtons(n){
 		$("#nextBtn").show();
 	}
 }
-
-// function loadTopology(topology_file) {
-//     console.log(topology_file);
-//     $.ajax({
-//         type: "GET",
-//         url: "makeGraph",
-//         data: {
-//             topology: topology_file,
-//         },
-//         dataType: "json",
-//         async: false,
-//         success: function (response) {
-//             if(response.success){
-//                 console.log("Graph of the topology is ready");
-//                 window.location.href='graph';
-//             }else{
-//                 alert("Could not graph the topology");
-//             }
-//         }
-//     });
-// }
