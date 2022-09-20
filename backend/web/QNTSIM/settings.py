f7b1b8038f76bf56d740cc7ed51e06d92fbd9a7f
Aman <aman.dabral.95@gmail.com>
2022-09-20 10:28:08 +0530

    Added frontend

--- backend/web/QNTSIM/settings.py
+++ backend/web/QNTSIM/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-he$()-i)($x%8kl#j*@%m($(p6=#t4!)cv8-w1=11f&iwx!fvx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["qntsim.qulabz.ai"]
+ALLOWED_HOSTS = ["localhost","0.0.0.0"]
 
 
 # Application definition
