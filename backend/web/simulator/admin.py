56b827fc9c25f2eede66fea3559746f552bad438
Aman <aman.dabral.95@gmail.com>
2022-10-18 15:52:21 +0530

    E91 and amin fix

--- backend/web/simulator/admin.py
+++ backend/web/simulator/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from simulator.models import Applications
+from simulator.models import Applications, Results
 
 # Register your models here.
 admin.site.register(Applications)
+admin.site.register(Results)
