050528bd0654b503acefa02b00e28b6f27e1af2b
PRATIK DAS <78647354+QuantumQoder@users.noreply.github.com>
2023-02-16 16:11:05 +0530

    included security check in __init__ file

--- backend/src/library/__init__.py
+++ backend/src/library/__init__.py
@@ -1,5 +1,6 @@
 from .network import Network, string_to_binary
 from .error_analyzer import ErrorAnalyzer
 from .protocol import Protocol
+from .security_checks import insert_check_bits, insert_decoy_photons
 
-__all__ = ['string_to_binary', 'Network', 'ErrorAnalyzer', 'Protocol']
+__all__ = ['string_to_binary', 'insert_check_bits', 'insert_decoy_photons', 'Network', 'ErrorAnalyzer', 'Protocol']
