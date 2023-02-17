8365b3414bb24b9243fed881de803b8881df634b
PRATIK DAS <78647354+QuantumQoder@users.noreply.github.com>
2023-02-17 11:47:54 +0530

    Added a script for common measurement circuits required in the comm. protocols

--- backend/src/library/__init__.py
+++ backend/src/library/__init__.py
@@ -2,5 +2,6 @@ from .network import Network, string_to_binary
 from .error_analyzer import ErrorAnalyzer
 from .protocol import Protocol
 from .security_checks import insert_check_bits, insert_decoy_photons
+from .circuits import bsm_circuit
 
-__all__ = ['string_to_binary', 'insert_check_bits', 'insert_decoy_photons', 'Network', 'ErrorAnalyzer', 'Protocol']
+__all__ = ['bsm_circuit', 'string_to_binary', 'insert_check_bits', 'insert_decoy_photons', 'Network', 'ErrorAnalyzer', 'Protocol']
