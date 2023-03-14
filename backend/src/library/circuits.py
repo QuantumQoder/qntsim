1a86566adb6703263c5233e0005c5ee6b3fcc0ae
PRATIK DAS <78647354+QuantumQoder@users.noreply.github.com>
2023-03-14 15:45:59 +0530

    Added noise model and minor changes

--- backend/src/library/circuits.py
+++ backend/src/library/circuits.py
@@ -1,6 +1,15 @@
 from ..components.circuit import QutipCircuit
 
-def bell_type_state_analyzer(num_qubits:int):
+def bell_type_state_analyzer(num_qubits:int) -> QutipCircuit:
+    """
+    Returns a QutipCircuit for analyzing a Bell-type state of a given number of qubits.
+
+    Args:
+        num_qubits (int): Number of qubits in the Bell-type state.
+
+    Returns:
+        QutipCircuit: QutipCircuit for analyzing the Bell-type state.
+    """
     qtc = QutipCircuit(num_qubits)
     for i in range(num_qubits-1, 0, -1): qtc.cx(0, i)
     qtc.h(0)
--- backend/src/library/security_checks.py
+++ backend/src/library/security_checks.py
@@ -14,12 +14,31 @@ def __insert_seq__(lst:List, seq:List):
     insert_locations = sample(range(len(lst)+len(seq)), len(seq))
     iter1, iter2 = iter(lst), iter(seq)
     
-    return [next(iter1) if i not in insert_locations else type(lst[0])(next(iter2)) if is_same_type else next(iter2) for i in range(len(lst)+len(seq))], insert_locations
+    return  insert_locations, [next(iter1) if i not in insert_locations else type(lst[0])(next(iter2)) if is_same_type else next(iter2) for i in range(len(lst)+len(seq))]
 
 def insert_check_bits(messages:List[str], num_check_bits:int):
-    return {tuple((seq:=__insert_seq__(lst=message, seq=randint(2, size=num_check_bits)))[1]):''.join(seq[0]) for message in messages}
+    """Insert check bits into a list of strings
+
+    Args:
+        messages (List[str]): Message, to insert check bits
+        num_check_bits (int): Number of check bits to be inserted
+
+    Returns:
+        modified message (Dict[Tuple[int], str]): The message 
+    """
+    return {tuple((seq:=__insert_seq__(lst=message, seq=randint(2, size=num_check_bits)))[0]):''.join(seq[1]) for message in messages}
 
 def insert_decoy_photons(network:Network, node_index:int, num_decoy_photons:int):
+    """Insert decoy photons at memory nodes of the network
+
+    Args:
+        network (Network): <Network> object
+        node_index (int): The node, to insert the decoy photons
+        num_decoy_photons (int): Number of decoy photons to be inserted
+
+    Returns:
+        keys, photons (List[int], numpy.array): The keys of the decoy photons, The photon states
+    """
     photons = randint(4, size=num_decoy_photons)
     photon_iterator = iter(photons)
     for info in network.nodes[node_index].resource_manager.memory_manager:
