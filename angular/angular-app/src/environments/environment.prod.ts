b297d63d60362a32e8e2e36d25676fbe98d922f8
Ubuntu <ubuntu@ip-172-31-2-132.ap-south-1.compute.internal>
2023-05-06 12:31:29 +0000

    enviroment

--- angular/angular-app/src/environments/environment.prod.ts
+++ angular/angular-app/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   // apiUrl: 'http://184.72.30.192:8000/'
-  apiUrl: 'http://65.1.13.72:8000/'
+  apiUrl: 'http://43.204.227.246:8000/',
+  apiUrlNew: 'http://43.204.227.213:8000/'
 };
 
--- backend/src/conf.py
+++ backend/src/conf.py
@@ -1,4 +1,4 @@
 import os
 
-#print("HARE", os.getcwd())
+print("HARE", os.getcwd())
 LOG_CONFIG_FILE = "/code/web/logging.ini"
