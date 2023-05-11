f265c130fabd5efbf2afc12bab00620f61dce2b8
SiddhiMali <siddhimali531@gmail.com>
2023-05-11 16:34:51 +0530

    updated logs

--- angular/angular-app/src/environments/environment.prod.ts
+++ angular/angular-app/src/environments/environment.prod.ts
@@ -2,8 +2,9 @@ export const environment = {
   production: true,
   // apiUrl: 'http://184.72.30.192:8000/'
   //apiUrl: 'http://43.204.227.246:8000/',
-  //: 'http://43.204.227.213:8000/'
-  apiUrl: 'http://0.0.0.0:8000/',
-  apiUrlNew: 'http://0.0.0.0:8000/'
+  apiUrl: 'http://43.204.227.246:8000/',
+  apiUrlNew: 'http://43.204.227.213:8000/'
+ // apiUrl: 'http://0.0.0.0:8000/',
+ // apiUrlNew: 'http://0.0.0.0:8000/'
 };
 
