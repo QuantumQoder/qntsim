387a5e3c818b4ce0ffbc8196234ef7174fd89edf
QuantumQoder <ratikdas63@gmail.com>
2023-05-22 12:43:33 +0000

    fixed api urls

--- angular/angular-app/src/environments/environment.ts
+++ angular/angular-app/src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   // apiUrl: 'http://184.72.30.192:8000/'
   // apiUrl: 'http://54.177.146.24:8000/'
   apiUrl: 'http://65.1.13.72:8000/',
-  apiUrlNew: 'http://65.1.13.72:8000/'
+  apiUrlNew: 'http://43.204.227.213:8000/',
   // apiUrl: 'http://0.0.0.0:8000/',
   // apiUrlNew: 'http://0.0.0.0:8000/'
 };
