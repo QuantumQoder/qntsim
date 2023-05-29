8bf8723804ca2988f099f25b17249a828023affe
harshasurya02 <harshasurya02@gmail.com>
2023-05-29 11:31:36 +0530

    Route Titles configured

--- angular/angular-app/src/app/app-routing.module.ts
+++ angular/angular-app/src/app/app-routing.module.ts
@@ -4,12 +4,12 @@ import { HomePageComponent } from './components/home-page/home-page.component';
 import { ResultsComponent } from './components/results/results.component';
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent },
-  { path: 'applications', loadChildren: () => import('./components/applications/application-modules/application.module').then(m => m.ApplicationModule) },
-  { path: 'advanced', loadChildren: () => import('./components/advanced/modules/advanced.module').then(m => m.AdvancedModule) },
-  { path: 'results', component: ResultsComponent },
-  { path: 'intro', loadChildren: () => import('./components/intro/intro-modules/intro.module').then(m => m.IntroModule) },
-  { path: 'minimal', loadChildren: () => import('./components/minimal/modules/minimal.module').then(m => m.MinimalModule) },
+  { path: '', component: HomePageComponent, title: "QNTSIM" },
+  { path: 'applications', loadChildren: () => import('./components/applications/application-modules/application.module').then(m => m.ApplicationModule), title: 'Applications' },
+  { path: 'advanced', loadChildren: () => import('./components/advanced/modules/advanced.module').then(m => m.AdvancedModule), title: 'Advanced' },
+  { path: 'results', component: ResultsComponent, title: 'Results' },
+  { path: 'intro', loadChildren: () => import('./components/intro/intro-modules/intro.module').then(m => m.IntroModule), title: "Intro" },
+  { path: 'minimal', loadChildren: () => import('./components/minimal/modules/minimal.module').then(m => m.MinimalModule), title: "Minimal" },
   { path: '**', redirectTo: '' }
 ];
 
