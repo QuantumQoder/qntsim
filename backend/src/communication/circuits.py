02414274dfa30d7944ad11d1d4c3c5b60c5e4244
QuantumQoder <ratikdas63@gmail.com>
2023-06-14 13:18:30 +0000

    added a xor_type state analyser

--- backend/src/communication/circuits.py
+++ backend/src/communication/circuits.py
@@ -1,5 +1,6 @@
 from ..components.circuit import QutipCircuit
 
+
 def bell_type_state_analyzer(num_qubits:int) -> QutipCircuit:
     """
     Returns a QutipCircuit for analyzing a Bell-type state of a given number of qubits.
@@ -11,8 +12,19 @@ def bell_type_state_analyzer(num_qubits:int) -> QutipCircuit:
         QutipCircuit: QutipCircuit for analyzing the Bell-type state.
     """
     qtc = QutipCircuit(num_qubits)
-    for i in range(num_qubits-1, 0, -1): qtc.cx(0, i)
+    for i in range(num_qubits-1, 0, -1):
+        qtc.cx(0, i)
     qtc.h(0)
-    for i in range(num_qubits): qtc.measure(i)
+    for i in range(num_qubits):
+        qtc.measure(i)
     
+    return qtc
+
+def xor_type_state_analyzer(num_qubits:int) -> QutipCircuit:
+    qtc = QutipCircuit(num_qubits)
+    for i in range(num_qubits-1):
+        qtc.cx(i, num_qubits-1)
+        qtc.h(i)
+    for i in range(num_qubits):
+        qtc.measure(i)
     return qtc
