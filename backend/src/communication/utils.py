8bf64feee83dc4e43f37e3e3dd44282293c948ec
QuantumQoder <ratikdas63@gmail.com>
2023-06-21 14:33:05 +0000

    added a pass_ (value) utility

--- backend/src/communication/utils.py
+++ backend/src/communication/utils.py
@@ -1,5 +1,5 @@
 import logging
-from typing import List
+from typing import Any, List
 
 
 def to_binary(messages: List[str]):
@@ -15,10 +15,10 @@ def to_binary(messages: List[str]):
     """
     # strings = [''.join('0'*(8-len(bin(ord(char))[2:]))+bin(ord(char))[2:] for char in message) for _, message in messages.items()]
     _is_binary = all(char in "01" for message in messages for char in message)
-    strings = [
+    strings = messages if _is_binary else [
         "".join("{:08b}".format(ord(char)) for char in message)
         for message in messages
-    ] if not _is_binary else messages
+    ]
     logging.info("converted")
 
     return _is_binary, strings
@@ -31,3 +31,6 @@ def to_string(strings:List[str], _was_binary:bool):
     logging.info("converted back")
 
     return messages
+
+def pass_(_: Any, returns: Any):
+    return returns
