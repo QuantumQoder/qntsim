7ccf9e9ae8f9faa40f6e3c1dec1a88e361da1f68
QuantumQoder <ratikdas63@gmail.com>
2023-06-21 14:34:48 +0000

    added pass_ utility to __init__

--- backend/src/communication/__init__.py
+++ backend/src/communication/__init__.py
@@ -6,9 +6,10 @@ from .noise import ERROR_TYPE
 from .NoiseModel import noise_model
 from .protocol import ProtocolPipeline
 from .security_checks import insert_check_bits, insert_decoy_photons
-from .utils import to_binary, to_string
+from .utils import pass_, to_binary, to_string
 
 __all__ = [
+    "pass_",
     "bell_type_state_analyzer",
     "insert_check_bits",
     "insert_decoy_photons",
@@ -20,5 +21,5 @@ __all__ = [
     "ErrorAnalyzer",
     "Network",
     "noise_model",
-    "ProtocolPipeline",
+    "ProtocolPipeline"
 ]
