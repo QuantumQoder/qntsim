12956248f0941341cad7835502e55f5945318b8b
PRATIK DAS <78647354+QuantumQoder@users.noreply.github.com>
2023-08-28 18:07:34 +0530

    added docstrings and type hints

--- backend/src/communication/utils.py
+++ backend/src/communication/utils.py
@@ -1,36 +1,46 @@
 import logging
-from typing import Any, List
+from typing import Any, Dict, List, Tuple, TypeAlias
 
+__msg_type:TypeAlias = Dict[Tuple[str], str]
 
-def to_binary(messages: List[str]):
+def to_binary(msg_dict:__msg_type) -> Tuple[bool, __msg_type]:
     """Converts any character string to their binary equivalent.
     Args:
-        messages (List[str]): List of messages to be converted into binary.
+        messages (List[str]): Messages to be converted into binary.
 
     Returns:
-        strings (List[str]): List of binary equivalent of the input messages
+        strings (List[str]): Binary equivalents of the input messages
 
     Example:
-        print(string_to_binary(messages=['h'])
+        # print(to_binary(messages={('n1', 'n2'):'h'})
     """
-    # strings = [''.join('0'*(8-len(bin(ord(char))[2:]))+bin(ord(char))[2:] for char in message) for _, message in messages.items()]
-    _is_binary = all(char in "01" for message in messages for char in message)
-    strings = messages if _is_binary else [
-        "".join("{:08b}".format(ord(char)) for char in message)
-        for message in messages
-    ]
+    __is_binary = all(char in "01" for message in msg_dict.values() for char in message)
+    binary_strings = msg_dict if __is_binary else {node_seq:"".join("{:08b}".format(ord(char)) for char in message) for node_seq, message in msg_dict.items()}
     logging.info("converted")
+    return __is_binary, binary_strings
 
-    return _is_binary, strings
+def to_characters(bin_dict:__msg_type, __was_binary:bool) -> __msg_type:
+    """Converts any binary string to their character equivalent.
 
-def to_string(strings:List[str], _was_binary:bool):
-    print(_was_binary)
-    messages = ["".join(
-                "{:c}".format(int(string[j * 8 : -~j * 8], 2)) for j in range(len(string) // 8)
-            ) for string in strings] if not _was_binary else strings
-    logging.info("converted back")
+    Args:
+        bin_dict (__msg_type): Binary strings to be converted into character strings
+        __was_binary (bool): If the input was previously binary.
+        NOTE: For an independent use of the function, set __was_binary = False
 
+    Returns:
+        __msg_type: Messages converted from the binary strings
+    """
+    messages = bin_dict if __was_binary else {node_seq:"".join("{:c}".format(int(binary[8*i:8*(i+1)], 2)) for i in range(len(binary)//8)) for node_seq, binary in bin_dict.items()}
     return messages
 
-def pass_returns(_: Any, returns: Any, *args, **kwds):
+def pass_values(_, returns:Any, *args, **kwds) -> Tuple[Any, Tuple[Any], Dict]:
+    """A do-nothing funciton which passes the arguments from the previous call to the next call
+
+    Args:
+        _ (_type_): Ignored arguement
+        returns (Any): Returns from the previous function call
+
+    Returns:
+        Tuple[Any, Tuple[Any], Dict]: Passes to the next function call
+    """
     return returns, args, kwds
