974a64c1b40e7a77c51fce577e95f05d3aeb717e
Pratik Das <ratikdas63@gmail.com>
2023-09-25 14:39:10 +0000

    added type hints and functions

--- backend/src/communication/security_checks.py
+++ backend/src/communication/security_checks.py
@@ -1,20 +1,27 @@
-from typing import List, Dict
 from random import sample
+from typing import Dict, List, Sequence, Tuple
+
 from numpy.random import randint
 
-from ..components.circuit import QutipCircuit
-from .network import Network
+from ..kernel.circuit import QutipCircuit
+from .qntsim import QNtSim
 
 # class SecurityCheck:
 #     @staticmethod
 #     def insert_check_bits(network:Network)
 
-def __insert_seq__(lst:List, seq:List):
+def __insert_seq__(lst:List, seq:List) -> Tuple[List[int], List]:
     is_same_type = all(type(lst[0])==type(ele) for ele in lst[1:])
     insert_locations = sample(range(len(lst)+len(seq)), len(seq))
     iter1, iter2 = iter(lst), iter(seq)
     
     return  insert_locations, [next(iter1) if i not in insert_locations else type(lst[0])(next(iter2)) if is_same_type else next(iter2) for i in range(len(lst)+len(seq))]
+
+def __separate_seq__(indices:Sequence, lst:Sequence) -> Tuple[List, List]:
+    list_items = list(filter(lambda x: x not in indices, lst))
+    seq_items = list(filter(lambda x: x in indices, lst))
+
+    return list_items, seq_items
 
 def insert_check_bits(messages:List[str], num_check_bits:int):
     """Insert check bits into a list of strings
@@ -28,7 +35,7 @@ def insert_check_bits(messages:List[str], num_check_bits:int):
     """
     return {tuple((seq:=__insert_seq__(lst=message, seq=randint(2, size=num_check_bits)))[0]):''.join(seq[1]) for message in messages}
 
-def insert_decoy_photons(network:Network, node_index:int, num_decoy_photons:int):
+def insert_decoy_photons(network:QNtSim, node_index:int, num_decoy_photons:int):
     """Insert decoy photons at memory nodes of the network
 
     Args:
@@ -54,3 +61,7 @@ def insert_decoy_photons(network:Network, node_index:int, num_decoy_photons:int)
             info.state = 'OCCUPIED'
     
     return [info.memory.qstate_key for info in network.nodes[node_index].resource_manager.memory_manager if info.state=='OCCUPIED'], photons
+
+# def insert_decoy_photons(interface:Interface, src_node:str, dst_node:str):
+#     photons = interface.photons.get(src_node).get(dst_node)
+#     insert_locs, new_photon_seq = __insert_seq__()
