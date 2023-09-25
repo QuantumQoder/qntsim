1ef6a282729aa08c117b410207189230b53a70ec
Pratik Das <ratikdas63@gmail.com>
2023-09-25 14:16:06 +0000

    generated circuit-results components

---
+++ angular/angular-app/src/app/components/circuit-results/circuit-results.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-circuit-results',
+  templateUrl: './circuit-results.component.html',
+  styleUrls: ['./circuit-results.component.less']
+})
+export class CircuitResultsComponent {
+
+}
