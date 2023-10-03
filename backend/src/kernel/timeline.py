87fdcb037fb0e718bf16c44cceb3566a0042b726
PRATIK DAS <78647354+QuantumQoder@users.noreply.github.com>
2023-10-03 19:44:29 +0530

    refactored timeline class

--- backend/src/kernel/timeline.py
+++ backend/src/kernel/timeline.py
@@ -4,84 +4,46 @@ This module defines the Timeline class, which provides an interface for the simu
 All entities are required to have an attached timeline for simulation.
 """
 
-from _thread import start_new_thread
-from math import inf
-from sys import stdout
-from time import time_ns, sleep
-from typing import TYPE_CHECKING
+import time
+from typing import TYPE_CHECKING, List, Literal
 
-from numpy import random
-from sympy import false
+from ..utils import log
+from .kernel_utils import EventList
+from .quantum_kernel import QuantumKernel
+from .quantum_manager import DensityManager
 
 if TYPE_CHECKING:
-    from ._event import Event
-
-from .kernel_utils import EventList
-from ..utils import log
-from .quantum_kernel import QuantumKernel
-from .quantum_manager import QuantumManagerDensity
-import logging
-# logger = logging.getLogger("main_logger." + "timeline")
-
-"""
-ch=input("Enter 0 for DLCZ ,1 for barettkok")
-if ch=='0':
-    DLCZ=True
-    bk=False
-    print("DLCZ input value",DLCZ)
-else:
-    DLCZ=False
-    bk=True
-    print("DLCZ input value",DLCZ)
-"""
-#DLCZ=False
-#bk=False 
-#if not DLCZ:
-    #bk=True
-#else:
-    #bk=False
+    from .entity import Entity
 
 
 class Timeline:
-    DLCZ=False
-    bk=True
+    DLCZ: bool = False
+    bk: bool = True
 
-
-    def __init__(self, stop_time=inf,backend=str, formalism='ket_vector'):
-        
+    def __init__(self, stop_time: float = -1, backend: Literal["qiskit", "qutip"] = "qutip", formalism: Literal["ket", "density"] = "ket") -> None:
         self.events = EventList()
-        self.entities = []
-        self.time = 0
+        self.entities: List["Entity"] = []
+        self.time: float = 0
         self.stop_time = stop_time
-        self.schedule_counter = 0
-        self.run_counter = 0
-        self.is_running = False
-        
-        self.type=backend
+        self.schedule_counter: int = 0
+        self.run_counter: int = 0
+        self._is_running = False
+        self.type = backend
+        match formalism:
+            case "ket": self.quantum_manager = QuantumKernel(backend)
+            case "density": self.quantum_manager = DensityManager()
+            case _: raise ValueError(f"Invalid formalism {formalism}")
 
-        if formalism == 'ket_vector':
-            #self.quantum_manager =QuantumKernel.create("Qutip")()
-            #print('timeline manager',QuantumKernel.create(self.type))
-            #self.quantum_manager =QuantumKernel.create(self.type)()
-            self.quantum_manager =QuantumKernel.create(self.type)()
-            #self.quantum_manager = QuantumManagerKet()
-        elif formalism == 'density_matrix':
-            self.quantum_manager = QuantumManagerDensity()
-        else:
-            raise ValueError(f"Invalid formalism {formalism}")
-
-    def now(self) -> int:
+    def now(self) -> float:
         """Returns current simulation time."""
-
         return self.time
 
     def init(self) -> None:
         """Method to initialize all simulated entities."""
         log.logger.info("Timeline initial network")
-        for entity in self.entities:
-            entity.init()
+        for entity in self.entities: entity.init()
 
-    def run(self) -> None:
+    def run(self, show_progress: bool = False) -> None:
         """Main simulation method.
 
         The `run` method begins simulation of events.
@@ -89,45 +51,16 @@ class Timeline:
         A progress bar may also be displayed, if the `show_progress` flag is set.
         """
         log.logger.info("Timeline start simulation")
-        
-        # logger.info("Timeline started simulation ")
-        tick = time_ns()
-        self.is_running = True
-
-        while len(self.events) > 0:
-        #while True:
-            event = self.events.pop()
-
-            if event.time >= self.stop_time:
-                #self.schedule(event)
-                self.schedule_counter += 1
-                self.events.push(event)
-                break
-
-            assert self.time <= event.time, f"invalid event time for process scheduled on {event.owner}"
-            
-            #if event.is_invalid():
-            if event._is_removed:
-                continue
-
-            self.time = event.time
-            
+        start: float = time.time()
+        self._is_running = True
+        for event in self.events:
             event.run()
-
+            self.time = event.time
             self.run_counter += 1
-            # print(f'The event-queue is: {self.events.filter_events()}')
-
-       
-        self.is_running = False
-        time_elapsed = time_ns() - tick
-        log.logger.info("Timeline end simulation. Execution Time: %d ns; Scheduled Event: %d; Executed Event: %d" %
-                        (time_elapsed, self.schedule_counter, self.run_counter))
+        self._is_running = False
+        stop: float = time.time()
 
     def stop(self) -> None:
         """Method to stop simulation."""
         log.logger.info("Timeline is stopped")
         self.stop_time = self.now()
-
-
-    
-    
